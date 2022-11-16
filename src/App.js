59eecfa7edfa147e12b335f36bf6df48b098a0e3
Abiodun Abolarinwa <47521066+biocell@users.noreply.github.com>
2022-11-16 11:50:43 +0100

    style: Home page sections

--- src/App.js
+++ src/App.js
@@ -11,18 +11,74 @@ function App() {
         <div className='left-column'>
         <h1 className='mb1'>Rent a <span className='gradient'>Place</span> away from <span className='gradient'>Home</span> in the <span className='gradient'>Metaverse</span></h1>
         <p className='mb1'>we provide you access to luxury and affordable houses in the metaverse, get a chance to turn your imagination to reality at your comfort zone</p>
-        <input className="search" type="text" placeholder="Search for location" /><p>Search</p>
+        
+      <form action="" id="search">
+          <input type="search" className="search" placeholder="Search for location" />
+          <button type="submit" className="search-btn">Search</button>
+      </form>
       </div>
-     
-     
-
       <div>
        <img src="./images/heroimgs.svg" alt="Hero images" />
       </div>
+     </div>
+     <div className="nav-2">
+      <div className='nav__container'>
+          <img src="./images/mbtoken.svg" alt="mbg token img" />
+          <img src="./images/metamask.svg" alt="meta mask img" />
+          <img src="./images/opensea.svg" alt="open sea img" />
       </div>
-    <div>
-      <p>Inspiration for your next adventure</p>
+     </div>
+     
+  <div id="inspiration">
+      <h1>Inspiration for your next adventure</h1>
+
+    <div className='grid repeat-4'>
+        <div>
+        <img src="./images/4048.svg" alt="dessert" />
+        </div>
+        <div>
+        <img src="./images/4049.svg" alt="dessert" />
+        </div>
+        <div>
+        <img src="./images/4050.svg" alt="dessert" />      
+        </div>
+        <div>
+        <img src="./images/4051.svg" alt="dessert" />
+        </div>
     </div>
+
+    <div className='grid repeat-4'>
+      <div >
+      <img src="./images/8040.svg" alt="dessert" />
+      </div>
+      <div>
+      <img src="./images/9440.svg" alt="dessert" />
+      </div>
+      <div>
+      <img src="./images/5040.svg" alt="dessert" />      
+      </div>
+      <div>
+      <img src="./images/5140.svg" alt="dessert" />
+      </div>
+    </div>
+
+  </div>
+
+  <div id="nft">
+    <div className="nft__section">
+        <h3>Metabnb NFTs</h3>
+          <p>
+           Discover our NFT gift cards collection. Loyal<br /> customers gets amazing
+           gift cards which are traded as NFTs. These NFTs gives our cutomer
+           access to loads of our exclusive services.
+          </p>
+      <button>Learn More</button>
+      
+    </div>
+    <div>
+      <img src="./images/nft.svg" alt=" nft" />
+    </div>
+  </div>
       <Footer />
     </div>
   );
